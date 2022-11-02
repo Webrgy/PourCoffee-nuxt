7b06ecd2f9b8b4d4c23b064d8aa099c31dc1a4af
Oussama Louelkadi <oussamalkd@pop-os.localdomain>
2022-11-02 17:44:30 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "~/assets/css/tailwind.scss"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
