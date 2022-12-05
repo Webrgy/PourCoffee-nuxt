9f4b8fca08b1c2b63afbeb895a8ca8ac02c14a90
Oussama <oussamalkd99@gmail.com>
2022-12-05 13:50:34 +0100

    feat: login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,9 +1,44 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { reactive } from 'vue';
 
-<template>
-  <div>
-    Page: Login
-  </div>
+  useHead({
+    title: "Login to your PourCoffee account"
+  })
+
+  const user = reactive({
+    email: "",
+    password: ""
+  })
+
+  const login = async() => {
+    const config = useRuntimeConfig()
+    await useFetch(`${config.baseUrl}/api/login`, {
+      method: "POST",
+      body: {
+        user : user
+      }
+    })
+  }
+
+
+
+</script>
+
+<template lang="pug">
+.login-page-container.px-2
+  .form-control.max-w-xl.mx-auto.w-full.rounded-box.shadow-xl.p-10.mx-5
+    h1.font-bold.text-xl.my-5.text-center(class="md:text-3xl") PourCoffee 
+      br
+      |Login To Your Account
+    span.text-center.text-base don't have an account? 
+      NuxtLink.link.text-primary(to="/register") Sign up
+    FormKit(type="form" submit-label="log in" @submit="login")
+      FormKit(v-model="user.email" validation="required|email" label="Email" type="email" name="email")
+      FormKit(v-model="user.password" validation="required" name="password" label="Password" type="password")
 </template>
 
-<style scoped></style>
+<style lang="scss">
+.login-page-container {
+  @apply w-full h-full mx-auto flex justify-center items-center;
+}
+</style>
--- pages/register.vue
+++ pages/register.vue
@@ -1,6 +1,9 @@
 <script lang="ts" setup>
-import { useSeoMeta } from "@unhead/vue";
 import { reactive } from "vue"
+
+  useHead({
+    title: "Register in PourCoffee now"
+  })
 
   const user = reactive({
     first_name: "",
@@ -10,20 +13,17 @@ import { reactive } from "vue"
     password_confirmation: ""
   })
 
-  const signup = /*async*/ (values: any) => {
-    /*const config = useRuntimeConfig()
+  const signup = async () => {
+    const config = useRuntimeConfig()
     await useFetch(`${config.baseUrl}/api/register`, {
       method: "post",
       body: {
         user: user
       }
-    })*/
-    // console.log(user.email, config.baseUrl);
+    }).then((res) => {
+      console.log(res);
+    })
   }
-  
-  useHead({
-    title: "Register in PourCoffee now"
-  })
 </script>
 
 <template lang="pug">
@@ -32,12 +32,14 @@ import { reactive } from "vue"
     h1.font-bold.text-xl.my-5.text-center(class="md:text-3xl") PourCoffee 
       br
       |Create Your Account
-    FormKit(type="form" submit-label="signup" @submit="signup")
-      FormKit(v-model="user.first_name" validation="required|alpha" name="first-name" label="First name" type="text")
-      FormKit(v-model="user.last_name" validation="required" name="last-name" label="Last name" type="text")
+    span.text-center.text-base already have an account? 
+      NuxtLink.link.text-primary(to="/login") Login
+    FormKit(type="form" submit-label="sign up" @submit="signup")
+      FormKit(v-model="user.first_name" validation="required|alpha|length:3,24" name="first-name" label="First name" type="text")
+      FormKit(v-model="user.last_name" validation="required|alpha|length:3,24" name="last-name" label="Last name" type="text")
       FormKit(v-model="user.email" validation="required|email" label="Email" type="email" name="email")
       FormKit(v-model="user.password" validation="required" name="password" label="Password" type="password")
-      FormKit(v-model="user.password_confirmation" validation="required" name="confirm-password" label="Confirm password" type="password")
+      FormKit(v-model="user.password_confirmation" validation="required|confirm:password" name="password_confirm" label="Confirm password" type="password")
   
 </template>
 
